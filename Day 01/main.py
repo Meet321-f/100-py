4ff4d36506fd5fac3a4b344de0e63494d3807746
Meet <tailorhetal622@gmail.com>
2026-08-02 20:06:13 +0530

    Day 01: Learn Python basics, variables, input/output and f-strings

---
+++ Day 01/main.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+my_name = "Meet"
+my_age = 21
+my_height = 5.3
+my_city = "Bilimora"
+
+print(my_name , my_age , my_height , my_city)
+# user name 
+user_name = input("enter your name: ")
+print("Hello" , user_name)
+
+# user age 
+user_age = int(input("enter your age : "))
+print("you are " , user_age , "year old")
+
+print("Welcome to our new project")
+

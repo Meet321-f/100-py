f9d7485201c3cea5fb985f3d11c67433efc8cf8c
Meet <tailorhetal622@gmail.com>
2026-08-02 20:08:28 +0530

    Day 01: Learn Python basics, variables, input/output and f-strings

--- Day 1/h1.py
+++
@@ -1,7 +0,0 @@
-user_name = input("Enter Your Name: ")
-user_age = int(input("Enter Your Age: "))
-user_city = input("Enter Your City: ")
-
-print(f"Hello {user_name}")
-print(f"You are {user_age} years old")
-print("You are live  in" , user_city)

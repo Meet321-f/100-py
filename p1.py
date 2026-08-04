5b68008ee7184e1907b8ef03d1e9efe451aea1d8
Meet <tailorhetal622@gmail.com>
2026-08-04 21:58:59 +0530

    leetcode problem find missing value

--- p1.py
+++ p1.py
@@ -18,3 +18,25 @@ def play_game():
 play_game()
 print("Game Over , Thanks For Playing")
 
+
+# Leetcode problem find missing numbers
+class Solution:
+    
+    def findMissingAndRepeatedValues(self, x):
+        
+        minimum = min(x)
+        maximum = max(x)
+
+        s = set(x)
+
+        ans = []
+
+        for i in range(minimum, maximum + 1):
+            if i not in s:
+                ans.append(i)
+
+        return ans
+sol = Solution()
+x = [1, 3, 2, 5]
+print(sol.findMissingAndRepeatedValues(x))
+print("Thank You.")

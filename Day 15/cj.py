9c31a6464b7ef391108fb8431040705772dc2aed
Meet <tailorhetal622@gmail.com>
2026-08-05 19:43:16 +0530

    Day 15: CSV File Handling completed

--- Day 15/cj.py
+++ Day 15/cj.py
@@ -15,59 +15,59 @@ CSV mean Each values separated by comma.
 
 import csv
 
-# data = [
-#     ["Name", "Age", "City"],
-#     ["writerows", 35, "Delhi"],
-#     ["Rohit", 22, "Mumbai"],
-#     ["Hardik", 31, "Surat"]
-# ]
+data = [
+    ["Name", "Age", "City"],
+    ["writerows", 35, "Delhi"],
+    ["Rohit", 22, "Mumbai"],
+    ["Hardik", 31, "Surat"]
+]
 
-# # if we wont to write onlt one row then we use writerow().
-# # if we wint to add mutiple rows in one time thet we use writerows().
+# if we wont to write onlt one row then we use writerow().
+# if we wint to add mutiple rows in one time thet we use writerows().
 
-# with open("s.csv" , "w" , newline="") as f:
-#     write = csv.writer(f)
+with open("s.csv" , "w" , newline="") as f:
+    write = csv.writer(f)
 
-#     write.writerows(data)
+    write.writerows(data)
 
-# print("CSV file created successfully")    
+print("CSV file created successfully")    
 
 
 
-# with open("s.csv" , "r" ) as f:
-#     reader  = csv.reader(f)
+with open("s.csv" , "r" ) as f:
+    reader  = csv.reader(f)
 
-#     for row in reader:
-#         print(row)
-#         print(type(row[1]))
+    for row in reader:
+        print(row)
+        print(type(row[1]))
 
 # print only name from the csv file 
-# with open("s.csv" , "r") as f:
-#     reader = csv.reader(f)
-#     next(reader) # Skip the header row
+with open("s.csv" , "r") as f:
+    reader = csv.reader(f)
+    next(reader) # Skip the header row
 
-#     for row in reader:
-#         print(row[0])
+    for row in reader:
+        print(row[0])
 
-# # print names and ages
-# with open("s.csv" , "r") as f:
-#    reader = csv.reader(f)
-#    next(reader)
+# print names and ages
+with open("s.csv" , "r") as f:
+   reader = csv.reader(f)
+   next(reader)
 
-#    for row in reader:
-#     print(row[0], "is", row[1] , "years old")
+   for row in reader:
+    print(row[0], "is", row[1] , "years old")
 
 
 # Diskreader 
-# with open("s.csv" , "r") as f:
-#     reader = csv.DictReader(f)
+with open("s.csv" , "r") as f:
+    reader = csv.DictReader(f)
 
-#     for row in reader:
-#         # print(row)        # print all the data in dictionary format
-#         # print(row["Name"])  # if we wont to print onky name 
-#         # print(row["Name"] , "Live in ", row["City"])
+    for row in reader:
+        # print(row)        # print all the data in dictionary format
+        # print(row["Name"])  # if we wont to print onky name 
+        # print(row["Name"] , "Live in ", row["City"])
 
-#        print(row["Name"], row["Age"], row["City"]) # print all the data in one line
+       print(row["Name"], row["Age"], row["City"]) # print all the data in one line
 
 # DictWriter()
 with open("s.csv" , "w" , newline="") as f:

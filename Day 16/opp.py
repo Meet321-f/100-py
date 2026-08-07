bb7bec1a59178f52d18a782fc1e9b3eb7686545a
Meet <tailorhetal622@gmail.com>
2026-08-07 20:43:50 +0530

    Day 16: Learned Python OOP basics, classes, objects, constructors, self, and attributes

--- Day 16/opp.py
+++ Day 16/opp.py
@@ -29,16 +29,48 @@ print(s1.marks)  # output: 85
 
 # example 
 
-# # class created 
-# class Car:
-#     color = "Blue"
-#     brand = "Toyota"
+# class created 
+class Car:
+    color = "Blue"
+    brand = "Toyota"
 
-# # who a object created
-# c1 = Car()
-# print(c1.color)  # output: Blue
-# print(c1.brand)  # output: Toyota    
+# who a object created
+c1 = Car()
+print(c1.color)  # output: Blue
+print(c1.brand)  # output: Toyota    
 
-# constructor = constructor is a special method which is used to initialize the object of class.
-#  _init_() = constructor method in python.
+constructor = constructor is a special method which is used to initialize the object of class.
+ _init_() = constructor method in python.
+
+class & instance attributes
+ 
+class Student:
+    collage_name = "Amtics"  # class attribute
+    def __init__(self , name , marks ):     # using th self perameter we store diffarent values to diffarent object.
+        self.name = name
+        self.marks = marks
+        print("Adding New Student in Database.")
+
+s1 = Student("Meet", 85)
+print(s1.name)  # output: meet
+print(s1.marks)  # output: 85
+print(s1.collage_name)  # output: Amtics
+
+
+class Student:
+    def __init__(self , name , marks ):     # using th self perameter we store diffarent values to diffarent object.
+        self.name = name
+        self.marks = marks
+
+    def welcome(self):
+        print("Welcome Student.", self.name)    
+
+    def get_marks(self):
+        return self.marks
+
+s1 = Student("Meet", 85)
+print(s1.name)  # output: meet
+print(s1.marks)  # output: 85
+s1.welcome()  # output: Welcome Student.
+print(s1.get_marks())  # output: 85
 

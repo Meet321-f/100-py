9613f7477c0b7fec704636ff8efe161a2ea4a38a
Meet <tailorhetal622@gmail.com>
2026-08-09 20:36:18 +0530

    Day 19 : inheritance

--- Day 19/oppskeyword.py
+++ Day 19/oppskeyword.py
@@ -2,63 +2,63 @@
 # del s1.name
 #del s1
 
-# class Student:
-#     def __init__(self , name):
-#         self.name = name
+class Student:
+    def __init__(self , name):
+        self.name = name
 
-# s1 = Student("Meet")
-# print(s1.name)
-# del s1
-# print(s1.name)
+s1 = Student("Meet")
+print(s1.name)
+del s1
+print(s1.name)
 
-# # inheritance : 
+# inheritance : 
 
-# # Example of simple inheritance
+# Example of simple inheritance
 
-# class Car:
-#     @staticmethod
-#     def start():
-#         print("Car started...")
+class Car:
+    @staticmethod
+    def start():
+        print("Car started...")
 
-#     @staticmethod
-#     def stop():
-#         print("Car stop.")
+    @staticmethod
+    def stop():
+        print("Car stop.")
 
-# class Toyotacar(Car):
-#      def __init__(self , name):
-#          self.name = name
+class Toyotacar(Car):
+     def __init__(self , name):
+         self.name = name
 
-# car1 = Toyotacar("Fortuner")
-# car2 = Toyotacar("Innova")
+car1 = Toyotacar("Fortuner")
+car2 = Toyotacar("Innova")
 
-# print(car1.name)
-# '''
-# if i try to do like this "print(car1.start())" 
-# here is not getting error because the first class or object
-#  is used for the secound class of object.
-# '''
-# print(car1.start())
+print(car1.name)
+'''
+if i try to do like this "print(car1.start())" 
+here is not getting error because the first class or object
+ is used for the secound class of object.
+'''
+print(car1.start())
 
-# # Multi level inheritance
-# class Car:
-#     @staticmethod
-#     def start():
-#         print("Car started...")
+# Multi level inheritance
+class Car:
+    @staticmethod
+    def start():
+        print("Car started...")
 
-#     @staticmethod
-#     def stop():
-#         print("Car stop.")
+    @staticmethod
+    def stop():
+        print("Car stop.")
 
-# class Toyotacar(Car):
-#      def __init__(self , brand):
-#          self.brand = brand
+class Toyotacar(Car):
+     def __init__(self , brand):
+         self.brand = brand
 
-# class Fortuner(Toyotacar):
-#     def __init__(self , type):
-#         self.type = type
+class Fortuner(Toyotacar):
+    def __init__(self , type):
+        self.type = type
 
-# car1 = Fortuner("electric")      
-# car1.start()  
+car1 = Fortuner("electric")      
+car1.start()  
 
 # multiple inheritance :
 class A:                          # class 1

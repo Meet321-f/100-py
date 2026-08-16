5be5cd1b94762e73f03d66af1367a33fef36d6e4
Meet <tailorhetal622@gmail.com>
2026-08-16 16:16:21 +0530

    prectice quations like calcuete marks and expance treker

---
+++ Day 26/expance.py
@@ -0,0 +1,37 @@
+name  = input("Enter Your Name : ")
+budget = float(input("Enter Your Budget : "))
+
+expense_count  = int(input("Enter Number of Expenses : "))
+
+print(f"Name : {name}")
+print(f"Budget : ₹{budget}")
+print(f"Number of Expenses : {expense_count}")
+
+expenses = []
+
+for i in range(expense_count):
+    print(f"\nExpense {i + 1}")
+             
+    expense_name = input(f"Enter Your Expense {i + 1} Name : ")
+    expense_category = input(f"Enter {expense_name} Category : ")
+    expense_amount = float(input(f"Enter {expense_name} Amount : ₹"))
+
+
+    expenses.append({
+        "name": expense_name,
+        "category": expense_category,
+        "amount": expense_amount
+    })
+
+total_expense = 0
+
+for expense in expenses:
+    total_expense += expense["amount"]
+
+remaining = budget - total_expense
+
+print("============summary============")
+print(f"Name : {name}")
+print(f"Budget : ₹{budget}")
+print(f"Total Expense : ₹{total_expense}")
+print(f"Remaining : ₹{remaining}")

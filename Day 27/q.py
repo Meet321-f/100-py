554b5edf4716425561d6bc05679c81f21f9c382a
Meet <tailorhetal622@gmail.com>
2026-08-18 20:25:35 +0530

    Day 27 solve  problem easy level

---
+++ Day 27/q.py
@@ -0,0 +1,39 @@
+# two pointer 
+
+s = "Hello World"
+s_list = list(s)
+
+left = 0
+right = len(s_list) -1
+
+while left < right:
+    s_list[left], s_list[right] = s_list[right], s_list[left]
+
+    left += 1
+    right -= 1
+
+resuklt = "".join(s_list)
+
+print("Original : Hello World")
+print("Modified : ", {resuklt})
+
+# Two Sum
+
+class Solution:
+    def twosum(self , nums : list[int] , target : int) -> list[int]:
+
+        seen ={}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
+
+        return []
+sol = Solution()
+nums = [2, 7 , 10, 15]
+target = 9
+print(sol.twosum(nums, target))
